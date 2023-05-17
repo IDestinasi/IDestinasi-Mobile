e7e33f9bdd60d0d25dd0186a2ebc96e66727633c
Arifki Ilham <79450046+Veroes@users.noreply.github.com>
2023-05-17 20:49:43 +0700

    added new tour picture to index

--- src/assets/image/_IndexImage.tsx
+++ src/assets/image/_IndexImage.tsx
@@ -3,9 +3,11 @@ import ImageIDestinasiWhite from './IDestinasiWhite.svg';
 import ImageProfile from './Profile.svg';
 
 const Tour1 = require('./Tour1.png');
+const Tour2 = require('./Tour2.png');
 export {
     ImageIDestinasi,
     ImageIDestinasiWhite,
     ImageProfile,
-    Tour1
+    Tour1,
+    Tour2
 }
