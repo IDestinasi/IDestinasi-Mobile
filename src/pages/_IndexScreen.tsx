2b624ce3b66524c11360a545e8c73454a88205ed
Arifki Ilham <79450046+Veroes@users.noreply.github.com>
2023-05-17 20:59:44 +0700

    add new modul in pages to index

--- src/pages/_IndexScreen.tsx
+++ src/pages/_IndexScreen.tsx
@@ -1,15 +1,9 @@
-import { Dimensions } from 'react-native';
-import Home from './HomeScreen';
+import Home from './user/HomeUserScreen';
 import Order from './OrderScreen';
 import Profile from './ProfileScreen';
 import Setting from './SettingScreen';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
 export {
-    windowWidth,
-    windowHeight,
     Home, 
     Order,
     Profile,
