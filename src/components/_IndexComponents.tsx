166ce5f70c06ee3090d632f6b58f24a405eb92d2
Arifki Ilham <79450046+Veroes@users.noreply.github.com>
2023-05-18 14:08:41 +0700

    add tour files to Index

--- src/components/_IndexComponents.tsx
+++ src/components/_IndexComponents.tsx
@@ -2,10 +2,14 @@ import BottomTab from "./BottomTab";
 import BottomTabNavigator from "./BottomTabNavigator";
 import HeaderInformation from './HeaderInformation';
 import TourCatalog from "./TourCatalog";
+import TourDiscovery from "./TourDiscovery";
+import TourPackets from "./TourPackets";
 
 export { 
     BottomTab,
     BottomTabNavigator,
     HeaderInformation,
-    TourCatalog 
+    TourCatalog,
+    TourDiscovery,
+    TourPackets
 };
